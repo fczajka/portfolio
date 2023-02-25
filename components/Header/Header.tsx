a8a4dbfe4b31bb92588baf2a2f0af6afcf12741c
fczajka <franekczajkaa@gmail.com>
2023-02-25 18:03:19 +0100

    Fix nav width

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { header } from "@/public/content";
 export default function Header() {
     const { menu, links } = header;
     return (
-        <header className="fixed hidden w-full h-16 bg-zinc-50 shadow-md shadow-zinc-200 z-20 lg:flex">
+        <header className="fixed hidden w-full h-16 bg-zinc-50 shadow-md shadow-zinc-200 z-20 lg:flex lg:justify-between">
             <div className="flex basis-1/2 pl-12 items-center h-full">
                 <button
                     className={`text-xl ${lilitaOne.variable} font-lilita-one tracking-wider`}
@@ -13,7 +13,7 @@ export default function Header() {
                     {menu}
                 </button>
             </div>
-            <nav className="basis-1/2">
+            <nav className="basis-1/2 max-w-5xl">
                 <ul className="flex items-center h-full">
                     {links.map((link) => (
                         <li key={link.text} className="basis-1/4 text-center">
