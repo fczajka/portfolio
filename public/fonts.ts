e9ec40c1571100a09212e3bc6b0fbe1d416f82ef
fczajka <franekczajkaa@gmail.com>
2023-03-30 23:18:40 +0200

    Add display swap to the fonts

--- public/fonts.ts
+++ public/fonts.ts
@@ -4,10 +4,12 @@ export const lilitaOne = Lilita_One({
     weight: "400",
     subsets: ["latin"],
     variable: "--font-lilita-one",
+    display: "swap",
 });
 
 export const sourceSansPro = Source_Sans_Pro({
     weight: "400",
     subsets: ["latin"],
     variable: "--font-source-sans-pro",
+    display: "swap",
 });
