b01627052ecffeca72caee7a1622fb5281122c0b
Franciszek Czajka <franekczajkaa@gmail.com>
2024-01-05 21:08:22 +0100

    Change config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    env: {
-        SERVICE_ID: process.env.SERVICE_ID,
-        TEMPLATE_ID: process.env.TEMPLATE_ID,
-        PUBLIC_KEY: process.env.PUBLIC_KEY,
-    },
+  env: {
+    SERVICE_ID: process.env.SERVICE_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    PUBLIC_KEY: process.env.PUBLIC_KEY,
+  },
 };
 
 module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
